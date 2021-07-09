5309c237bde0f5e5a56c2f3e33a4c69d850f3d86
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-07-09 15:52:59 +0200

    #1 zadanie 1, wybór operacji zależnie od opcji

---
+++ src/calculator.java
@@ -0,0 +1,24 @@
+public class calculator {
+    public static void main(String[] args) {
+        double a = 6.5;
+        double b = 7;
+        char option = '+';
+        switch(option){
+            case '+':
+                System.out.println(a+b);
+                break;
+            case '-':
+                    System.out.println(a-b);
+                    break;
+            case '*':
+                System.out.println(a*b);
+            case '/':
+                        System.out.println(a/b);
+                        break;
+
+
+
+
+        }
+    }
+}
