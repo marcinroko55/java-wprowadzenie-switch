6f08c86a21fc677f61866e9d480a83fd37e8ca05
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-07-19 21:59:01 +0200

    #5 wprowadzono poprawki do działania polegającego na wyliczaniu procentu podwyżki

--- src/salaryAndRaise.java
+++ src/salaryAndRaise.java
@@ -4,8 +4,8 @@ import java.sql.SQLOutput;
 public class salaryAndRaise {
     public static void main(String[] args) {
 
-        int salary = 2000;
-        int salaryAfterRaise = salary;
+        int salary = 2500;
+        double salaryAfterRaise = salary;
         System.out.println("Pensja wynosiła: " + salary);
 
         switch (salary) {
@@ -23,7 +23,7 @@ public class salaryAndRaise {
 
         }
         System.out.println("Pensja po podwyżce wynosi " + salaryAfterRaise);
-        System.out.println("Pensja wzrosła o " + ((salaryAfterRaise / salary) * 100 - 100) + "%");
+        System.out.println("Pensja wzrosła o " + (((salaryAfterRaise - salary) / salary) * 100) + "%");
 
 
     }
