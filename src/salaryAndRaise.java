c32256508ee59c1183e035f9c07f6c177ec3afc1
marcinroko55 <79092872+marcinroko55@users.noreply.github.com>
2021-07-19 21:36:30 +0200

    #5 rozwiązanie zadania

---
+++ src/salaryAndRaise.java
@@ -0,0 +1,30 @@
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
+public class salaryAndRaise {
+    public static void main(String[] args) {
+
+        int salary = 2000;
+        int salaryAfterRaise = salary;
+        System.out.println("Pensja wynosiła: " + salary);
+
+        switch (salary) {
+            case 100:
+                salaryAfterRaise = salary + 1000;
+                break;
+            case 2000:
+                int raise2 = 200;
+                salaryAfterRaise = salary + 200;
+                break;
+            case 2500:
+                int raise3 = 25;
+                salaryAfterRaise = salary + 25;
+                break;
+
+        }
+        System.out.println("Pensja po podwyżce wynosi " + salaryAfterRaise);
+        System.out.println("Pensja wzrosła o " + ((salaryAfterRaise / salary) * 100 - 100) + "%");
+
+
+    }
+}
